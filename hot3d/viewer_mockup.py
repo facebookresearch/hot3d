226f1ab7af1a5017b648b1896359d2c737ab716a
Pierre Moulon <pierrem@meta.com>
2024-02-09 13:44:39 -0800

    Mockup viewer
    Summary: Mockup viewer to see how usable the data provider API could be to make a visualization app with rerun
    
    Reviewed By: prithvirb
    
    Differential Revision: D53242535
    
    fbshipit-source-id: 801def54e71946b4a467aaabdfc4486649a49160

---
+++ hot3d/viewer_mockup.py
@@ -0,0 +1,92 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+import rerun as rr
+from projectaria_tools.utils.rerun_helpers import ToTransform3D
+
+from tqdm import tqdm
+
+from .dataset_api import DeviceType, Hot3DDataProvider
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--folder",
+        type=str,
+        help="path to hot3d data sequence",
+    )
+
+
+def main():
+    args = parse_args()
+
+    #
+    # Gather data input
+    #
+    if args.folder is None:
+        print("Nothing to display.")
+        exit(1)
+
+    # Initialize hot3d data provider
+    data_provider = Hot3DDataProvider(args.folder)
+
+    # TODO:
+    # For convenience LOG the camera trajectory as a 3D line to help user understand the type of motion in the sequence
+
+    #
+    # Visualize the dataset sequence
+    #
+    # Loop over the timestamps of the sequence and visualize corresponding data
+    timestamps = data_provider.get_timestamps()
+    for timestamp_us in tqdm(timestamps):
+
+        # Retrieve METADATA object and visualize them
+
+        # Plot image
+        image_data = data_provider.get_image(timestamp_us)
+        rr.log(
+            f"world/device/{image_data.label}",
+            rr.Image(image_data.img).compress(jpeg_quality=args.jpeg_quality),
+        )
+
+        # Plot Hand poses
+        hands_data = data_provider.get_hand_poses(timestamp_us)
+        for hand_data in hands_data:
+            print(f"Hand pose: {hand_data}")
+
+        # Plot Object poses
+        objects_data = data_provider.get_object_poses(timestamp_us)
+        for object_data in objects_data:
+            print(f"Object pose: {object_data}")
+
+            rr.log(f"/world/{object_data.uid}", ToTransform3D(object_data.pose, False))
+
+            # If desired (display the corresponding 3D object)
+
+        # Plot device pose
+        device_pose_data = data_provider.get_device_pose(timestamp_us)
+        T_world_device = device_pose_data.T_world_device
+        rr.log("world/device", ToTransform3D(T_world_device, False))
+
+        # Deal with device specifics
+        if data_provider.get_device_type() == DeviceType.ARIA:
+            print("MPS specifics")
+            # Display MPS artefact if desired
+
+
+if __name__ == "__main__":
+    main()
