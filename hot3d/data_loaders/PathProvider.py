4f49bd0649d84cfe7e2e1c5da579715f3309384e
Pierre Moulon <pierrem@meta.com>
2024-06-20 11:19:37 -0700

    Refactoring PathProvider
    Summary:
    Refactor PathProvider.py to use inheritance, aka.
    i.e AriaDatasetPaths and QuestDatasetPaths inherit from  SequenceDatasetPathsBase and remove some duplicated definition of properties
    
    Reviewed By: prithvirb
    
    Differential Revision: D58632000
    
    fbshipit-source-id: 0e453d3da1e59f90bd72f2ecb9a9426d45c92642

--- hot3d/data_loaders/PathProvider.py
+++ hot3d/data_loaders/PathProvider.py
@@ -13,32 +13,18 @@
 # limitations under the License.
 
 import os
+from abc import abstractmethod
 
 from .headsets import Headset
 from .io_utils import load_json
 
 
-class Hot3dDataPathProvider(object):
-    @staticmethod
-    def fromRecordingFolder(recording_instance_folderpath):
+class SequenceDatasetPathsBase:
+    """
+    Class defining the expected filepaths for a given HOT3D sequence
+    This base class can be extended to support headset specific filepaths (Aria, Quest, etc.)
+    """
 
-        metadata_filepath = os.path.join(recording_instance_folderpath, "metadata.json")
-        metadata_json = load_json(metadata_filepath)
-        headset = Headset[metadata_json["headset"]]
-
-        if headset is Headset.Aria:
-            return AriaDatasetPaths(
-                recording_instance_folderpath=recording_instance_folderpath
-            )
-        elif headset is Headset.Quest3:
-            return Quest3DatasetPaths(
-                recording_instance_folderpath=recording_instance_folderpath
-            )
-        else:
-            raise NotImplementedError(f"{headset} not supported at the moment.")
-
-
-class Quest3DatasetPaths(object):
     def __init__(self, recording_instance_folderpath):
         self._recording_instance_folderpath = recording_instance_folderpath
 
@@ -73,6 +59,37 @@ class Quest3DatasetPaths(object):
     @property
     def scene_metadata_filepath(self):
         return f"{self._recording_instance_folderpath}/metadata.json"
+
+    @abstractmethod
+    def is_valid(self) -> bool:
+        """
+        Returns if the list of required file for the Headset sequence are available
+        """
+
+
+class Hot3dDataPathProvider(object):
+    @staticmethod
+    def fromRecordingFolder(recording_instance_folderpath) -> SequenceDatasetPathsBase:
+
+        metadata_filepath = os.path.join(recording_instance_folderpath, "metadata.json")
+        metadata_json = load_json(metadata_filepath)
+        headset = Headset[metadata_json["headset"]]
+
+        if headset is Headset.Aria:
+            return AriaDatasetPaths(
+                recording_instance_folderpath=recording_instance_folderpath
+            )
+        elif headset is Headset.Quest3:
+            return Quest3DatasetPaths(
+                recording_instance_folderpath=recording_instance_folderpath
+            )
+        else:
+            raise NotImplementedError(f"{headset} not supported at the moment.")
+
+
+class Quest3DatasetPaths(SequenceDatasetPathsBase):
+    def __init__(self, recording_instance_folderpath):
+        super().__init__(self, recording_instance_folderpath)
 
     @property
     def camera_models_filepath(self):
@@ -92,45 +109,13 @@ class Quest3DatasetPaths(object):
         return all(os.path.exists(filepath) for filepath in self.required_filepaths)
 
 
-class AriaDatasetPaths(object):
+class AriaDatasetPaths(SequenceDatasetPathsBase):
     def __init__(self, recording_instance_folderpath):
-        self._recording_instance_folderpath = recording_instance_folderpath
-
-    @property
-    def recording_instance_folderpath(self):
-        return self._recording_instance_folderpath
-
-    @property
-    def dynamic_objects_filepath(self):
-        return f"{self._recording_instance_folderpath}/dynamic_objects.csv"
-
-    @property
-    def headset_trajectory_filepath(self):
-        return f"{self._recording_instance_folderpath}/headset_trajectory.csv"
-
-    @property
-    def mano_hand_pose_trajectory_filepath(self):
-        return f"{self._recording_instance_folderpath}/mano_hand_pose_trajectory.jsonl"
-
-    @property
-    def vrs_filepath(self):
-        return f"{self._recording_instance_folderpath}/recording.vrs"
-
-    @property
-    def box2d_objects_filepath(self):
-        return f"{self._recording_instance_folderpath}/box2d_objects.csv"
-
-    @property
-    def box2d_hands_filepath(self):
-        return f"{self._recording_instance_folderpath}/box2d_hands.csv"
+        super().__init__(recording_instance_folderpath)
 
     @property
     def mps_folderpath(self):
         return f"{self._recording_instance_folderpath}/mps"
-
-    @property
-    def scene_metadata_filepath(self):
-        return f"{self._recording_instance_folderpath}/metadata.json"
 
     @property
     def required_filepaths(self):
