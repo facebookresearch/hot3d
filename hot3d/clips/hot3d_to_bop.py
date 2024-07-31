dcec01b2869fcb484c43f65c1b8a3a8b1452690c
Anas Gouda <anas.gouda@tu-dortmund.de>
2024-08-01 00:49:31 +0200

    BOP format: generate annotations for all camera streams

--- hot3d/clips/hot3d_to_bop.py
+++ hot3d/clips/hot3d_to_bop.py
@@ -30,6 +30,10 @@ def main():
     parser.add_argument("--output_dataset_path", type=str, default="/media/gouda/ssd_data/datasets/hot3d/hot3d/train_aria_subsample_bop")
     # object models directory
     parser.add_argument("--object_models_dir", type=str, default="/media/gouda/ssd_data/datasets/hot3d/hot3d/object_models_eval")
+    # list of camera streams
+    parser.add_argument("--camera_streams_id", type=str, nargs="+", default=["214-1", "1201-1", "1201-2"])
+    # list of camera streams names
+    parser.add_argument("--camera_streams_names", type=str, nargs="+", default=["rgb", "gray1", "gray2"])
 
     args = parser.parse_args()
 
@@ -57,6 +61,7 @@ def main():
 
     # loop over all clips
     for clip in split_clips:  # TODO add tqdm
+        print(f"Processing clip: {clip}")
         # get clip id
         clip_name = clip.split(".")[0].split("-")[1]
 
@@ -67,38 +72,44 @@ def main():
         scene_output_dir = os.path.join(scenes_output_dir, clip_name)
         os.makedirs(scene_output_dir, exist_ok=True)
 
-        rgb_dir = os.path.join(scene_output_dir, "rgb")
-        os.makedirs(rgb_dir, exist_ok=True)
-        mask_dir = os.path.join(scene_output_dir, "mask")
-        os.makedirs(mask_dir, exist_ok=True)
-        mask_visib_dir = os.path.join(scene_output_dir, "mask_visib")
-        os.makedirs(mask_visib_dir, exist_ok=True)
+        # make path of folders and folders
+        # eg: STREAM_NAME, mask_STREAM_NAME, mask_visib_STREAM_NAME
+        # also create path for each json file
+        # eg: scene_camera_STREAM_NAME.json, scene_gt_STREAM_NAME.json, scene_gt_info_STREAM_NAME.json
+        # create a dictionary for all camera streams
+        clip_stream_paths = {}
+        for stream_name in args.camera_streams_names:
+            # directories
+            stream_image_dir = os.path.join(scene_output_dir, stream_name)
+            os.makedirs(stream_image_dir, exist_ok=True)
+            clip_stream_paths[stream_name] = stream_image_dir
+            stream_mask_dir = os.path.join(scene_output_dir, f"mask_{stream_name}")
+            os.makedirs(stream_mask_dir, exist_ok=True)
+            clip_stream_paths[f"mask_{stream_name}"] = stream_mask_dir
+            stream_mask_visib_dir = os.path.join(scene_output_dir, f"mask_visib_{stream_name}")
+            os.makedirs(stream_mask_visib_dir, exist_ok=True)
+            clip_stream_paths[f"mask_visib_{stream_name}"] = stream_mask_visib_dir
+            # json files
+            stream_scene_camera_json_path = os.path.join(scene_output_dir, f"scene_camera_{stream_name}.json")
+            clip_stream_paths[f"scene_camera_{stream_name}"] = stream_scene_camera_json_path
+            stream_scene_gt_json_path = os.path.join(scene_output_dir, f"scene_gt_{stream_name}.json")
+            clip_stream_paths[f"scene_gt_{stream_name}"] = stream_scene_gt_json_path
+            stream_scene_gt_info_json_path = os.path.join(scene_output_dir, f"scene_gt_info_{stream_name}.json")
+            clip_stream_paths[f"scene_gt_info_{stream_name}"] = stream_scene_gt_info_json_path
 
-        scene_gt_json_path = os.path.join(scene_output_dir, "scene_gt.json")
-        scene_gt_info_json_path = os.path.join(scene_output_dir, "scene_gt_info.json")
-        scene_camera_json_path = os.path.join(scene_output_dir, "scene_camera.json")
-        #scene_gt_info_json_path = os.path.join(args.dataset_path, args.split, "scene_gt_info.json")  # TODO if cannot be generated with BOP toolkit create it in this script.
-
-        # create scene_gt_info.json
+        # make a dict of dicts with stream name as keys
+        scene_camera_data = {}
         scene_gt_data = {}
         scene_gt_info_data = {}
-        scene_camera_data = {}
-        #"scene_gt_info" = {}  # TODO if cannot be generated with BOP toolkit create it in this script.
-        
+        for stream_name in args.camera_streams_names:
+            # add an empty dict indicating the stream name
+            scene_camera_data[stream_name] = {}
+            scene_gt_data[stream_name] = {}
+            scene_gt_info_data[stream_name] = {}
+
         # loop over all frames
         for frame_id in tqdm(range(clip_util.get_number_of_frames(tar))):
             frame_key = f"{frame_id:06d}"
-
-            # get rgb image from the clip tar file
-            rgb_image = clip_util.load_image(tar, frame_key, RGB_STREAM)
-            # convert to BGR
-            rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
-            # rotate the image 90 degrees
-            rgb_image = np.rot90(rgb_image, k=3)
-            # save rgb image
-            cv2.imwrite(os.path.join(rgb_dir, frame_key+".jpg"), rgb_image)
-
-            # filling scene_gt data
 
             # Load camera parameters.
             # from FRAME_ID.cameras.json
@@ -106,126 +117,141 @@ def main():
             ## read FRAME_ID.objects.json
             frame_objects = clip_util.load_object_annotations(tar, frame_key)
 
-            # get T_world_from_camera
-            T_camera_to_world = frame_camera[RGB_STREAM].T_world_from_eye
-
-            # TODO check that inverting this homogenous transformation is correct
-            # get T_world_2_camera as in the BOP format
-            T_world_to_camera = np.eye(4)
-            T_world_to_camera[:3, :3] = T_camera_to_world[:3, :3]
-            T_world_to_camera[:3, 3] = -T_camera_to_world[:3, :3].T @ T_camera_to_world[:3, 3]
-
             # TODO can i read this from the camera model?
-            # read max_solid_angle and projection_params from FRAME_ID.cameras.json
+            # read calibration json as it is
             camera_json_file_name = f"{frame_id:06d}.cameras.json"
             camera_json_file = tar.extractfile(camera_json_file_name)
             frame_camera_data = json.load(camera_json_file)
-            # get camera parameters
-            calibration = frame_camera_data[RGB_STREAM]["calibration"]
 
-            # add frame scene_camera data
-            scene_camera_data[int(frame_id)] = {
-                # TODO change this after we agree on the final format
-                "cam_model": calibration,
-                #"cam_K":  # not used as cam_model exists
-                #"depth_scale": 1.0,  # also not used
-                "cam_t_w2c": T_world_to_camera[:3, 3].flatten().tolist(),
-                "cam_R_w2c": T_world_to_camera[:3, :3].flatten().tolist(),
-            }
+            # loop over all camera streams
+            for stream_index, stream_name in enumerate(args.camera_streams_names):
+                stream_id = args.camera_streams_id[stream_index]
 
-            # Camera parameters of the current image.
-            camera_model = frame_camera[RGB_STREAM]
+                # load the image corresponding to the stream and frame
+                image = clip_util.load_image(tar, frame_key, stream_id)
+                # if image is rgb (3 channels), convert to BGR
+                if image.ndim == 3:
+                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                # save the image
+                image_path = os.path.join(clip_stream_paths[stream_name], frame_key+".jpg")
+                cv2.imwrite(image_path, image)
 
-            frame_scene_gt_data = []
-            frame_scene_gt_info_data = []
-            # loop with enumerate over all objects in the frame
-            for anno_id, obj_key in enumerate(frame_objects):
-                obj_data = frame_objects[obj_key][0]
-                bop_id = int(obj_data["object_bop_id"])
+                # filling scene_camera.json
 
-                # Transformation from the model to the world space.
-                T_object_to_world = clip_util.se3_from_dict(obj_data["T_world_from_object"])
+                # get T_world_from_camera
+                T_camera_to_world = frame_camera[stream_id].T_world_from_eye
 
-                # get object pose in camera frame
-                T_camera_to_object = T_world_to_camera @ T_object_to_world
+                # TODO check that inverting this homogenous transformation is correct
+                # get T_world_2_camera as in the BOP format
+                T_world_to_camera = np.eye(4)
+                T_world_to_camera[:3, :3] = T_camera_to_world[:3, :3]
+                T_world_to_camera[:3, 3] = -T_camera_to_world[:3, :3].T @ T_camera_to_world[:3, 3]
 
-                object_frame_scene_gt_anno = {
-                    "obj_id": obj_key,
-                    "cam_R_m2c": T_camera_to_object[:3, :3].flatten().tolist(),
-                    "cam_t_m2c": T_camera_to_object[:3, 3].flatten().tolist(),
+                # get camera parameters
+                calibration = frame_camera_data[stream_id]["calibration"]
+
+                # add frame scene_camera data
+                scene_camera_data[stream_name][int(frame_id)] = {
+                    "scene_camera": {
+                        # TODO change this after we agree on the final format
+                        "cam_model": calibration,
+                        #"cam_K":  # not used as cam_model exists
+                        #"depth_scale":  # also not used
+                        "cam_t_w2c": T_world_to_camera[:3, 3].flatten().tolist(),
+                        "cam_R_w2c": T_world_to_camera[:3, :3].flatten().tolist(),
+                    }
                 }
 
-                # Transformation from the model to the world space.
-                T = clip_util.se3_from_dict(obj_data["T_world_from_object"])
+                # Camera parameters of the current image.
+                camera_model = frame_camera[stream_id]
 
-                # Vertices in the model space.
-                verts_in_m = object_models[bop_id].vertices
+                frame_scene_gt_data = []
+                frame_scene_gt_info_data = []
+                # loop with enumerate over all objects in the frame
+                for anno_id, obj_key in enumerate(frame_objects):
+                    obj_data = frame_objects[obj_key][0]
+                    bop_id = int(obj_data["object_bop_id"])
 
-                # Vertices in the world space (can be brought to the camera
-                # space by the inverse of camera_model.T_world_from_eye).
-                verts_in_w = (T[:3, :3] @ verts_in_m.T + T[:3, 3:]).T
+                    # Transformation from the model to the world space.
+                    T_object_to_world = clip_util.se3_from_dict(obj_data["T_world_from_object"])
 
-                # Render the object model (outputs: rgb, mask, depth).
-                _, mask, _ = rasterizer.rasterize_mesh(
-                    verts=verts_in_w,
-                    faces=object_models[bop_id].faces,
-                    vert_normals=object_models[bop_id].vertex_normals,
-                    camera=camera_model,
-                )
+                    # get object pose in camera frame
+                    T_camera_to_object = T_world_to_camera @ T_object_to_world
 
-                # if no pixel is one in the mask, skip this object
-                if np.count_nonzero(mask) == 0:
-                    continue
+                    object_frame_scene_gt_anno = {
+                        "obj_id": obj_key,
+                        "cam_R_m2c": T_camera_to_object[:3, :3].flatten().tolist(),
+                        "cam_t_m2c": T_camera_to_object[:3, 3].flatten().tolist(),
+                    }
 
-                mask *= 255
-                # rotate the mask 90 degrees
-                mask = np.rot90(mask, k=3)
+                    # Transformation from the model to the world space.
+                    T = clip_util.se3_from_dict(obj_data["T_world_from_object"])
 
-                anno_id = f"{anno_id:06d}"
+                    # Vertices in the model space.
+                    verts_in_m = object_models[bop_id].vertices
 
-                # save mask FRAME-ID_ANNO-ID.png
-                mask_path = os.path.join(mask_dir, frame_key+"_"+anno_id+".png")
-                # save mask
-                cv2.imwrite(mask_path, mask)
-                # TODO for now save mask as mask_visib - change it later after generating the correct mask_visib
-                # save mask_visib FRAME-ID_ANNO-ID.png
-                mask_visib_path = os.path.join(mask_visib_dir, frame_key+"_"+anno_id+".png")
-                # save mask_visib
-                cv2.imwrite(mask_visib_path, mask)
+                    # Vertices in the world space (can be brought to the camera
+                    # space by the inverse of camera_model.T_world_from_eye).
+                    verts_in_w = (T[:3, :3] @ verts_in_m.T + T[:3, 3:]).T
 
-                # add scene_gt_info data
-                ## calculate bbox from mask with cv2 (x, y, width, height
-                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                x, y, w, h = cv2.boundingRect(contours[0])
-                bbox_obj = [x, y, w, h]
-                bbox_visib = bbox_obj  # TODO change to visib mask after getting it for Hot3D
-                px_count_all = cv2.countNonZero(mask)
-                px_count_visib = px_count_all  # TODO change to visib mask after getting it for Hot3D
-                visib_fract = px_count_visib / px_count_all
-                object_frame_scene_gt_info_anno = {
-                    "bbox_obj": bbox_obj,
-                    "bbox_visib": bbox_visib,
-                    "px_count_all": px_count_all,
-                    "px_count_valid": px_count_all,  # excluded as Hot3D is RGB only
-                    "px_count_visib": px_count_visib,
-                    "visib_fract": visib_fract,
-                }
+                    # Render the object model (outputs: rgb, mask, depth).
+                    _, mask, _ = rasterizer.rasterize_mesh(
+                        verts=verts_in_w,
+                        faces=object_models[bop_id].faces,
+                        vert_normals=object_models[bop_id].vertex_normals,
+                        camera=camera_model,
+                    )
 
-                frame_scene_gt_data.append(object_frame_scene_gt_anno)
-                frame_scene_gt_info_data.append(object_frame_scene_gt_info_anno)
+                    # if no pixel is one in the mask, skip this object
+                    if np.count_nonzero(mask) == 0:
+                        continue
 
-            scene_gt_data[int(frame_id)] = frame_scene_gt_data
-            scene_gt_info_data[int(frame_id)] = frame_scene_gt_info_data
+                    mask *= 255
 
-        # save scene_gt.json
-        with open(scene_gt_json_path, "w") as f:
-            json.dump(scene_gt_data, f, indent=2)
-        # save scene_gt_info.json
-        with open(scene_gt_info_json_path, "w") as f:
-            json.dump(scene_gt_info_data, f, indent=2)
-        # save scene_camera.json
-        with open(scene_camera_json_path, "w") as f:
-            json.dump(scene_camera_data, f, indent=2)
+                    anno_id = f"{anno_id:06d}"
+
+                    # save mask FRAME-ID_ANNO-ID.png
+                    mask_path = os.path.join(clip_stream_paths[f"mask_{stream_name}"], frame_key+"_"+anno_id+".png")
+                    # save mask
+                    cv2.imwrite(mask_path, mask)
+                    # TODO for now save mask as mask_visib - change it later after generating the correct mask_visib
+                    # save mask_visib FRAME-ID_ANNO-ID.png
+                    mask_visib_path = os.path.join(clip_stream_paths[f"mask_visib_{stream_name}"], frame_key+"_"+anno_id+".png")
+                    # save mask_visib
+                    cv2.imwrite(mask_visib_path, mask)
+
+                    # add scene_gt_info data
+                    ## calculate bbox from mask with cv2 (x, y, width, height
+                    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    x, y, w, h = cv2.boundingRect(contours[0])
+                    bbox_obj = [x, y, w, h]
+                    bbox_visib = bbox_obj  # TODO change to visib mask after getting it for Hot3D
+                    px_count_all = cv2.countNonZero(mask)
+                    px_count_visib = px_count_all  # TODO change to visib mask after getting it for Hot3D
+                    visib_fract = px_count_visib / px_count_all
+                    object_frame_scene_gt_info_anno = {
+                        "bbox_obj": bbox_obj,
+                        "bbox_visib": bbox_visib,
+                        "px_count_all": px_count_all,
+                        "px_count_valid": px_count_all,  # excluded as Hot3D is RGB only
+                        "px_count_visib": px_count_visib,
+                        "visib_fract": visib_fract,
+                    }
+
+                    frame_scene_gt_data.append(object_frame_scene_gt_anno)
+                    frame_scene_gt_info_data.append(object_frame_scene_gt_info_anno)
+
+                scene_gt_data[stream_name][int(frame_id)] = frame_scene_gt_data
+                scene_gt_info_data[stream_name][int(frame_id)] = frame_scene_gt_info_data
+
+        # save scene_gt.json, scene_gt_info.json, scene_camera.json for each camera stream
+        for stream_name in args.camera_streams_names:
+            with open(clip_stream_paths[f"scene_camera_{stream_name}"], "w") as f:
+                json.dump(scene_camera_data[stream_name], f, indent=4)
+            with open(clip_stream_paths[f"scene_gt_{stream_name}"], "w") as f:
+                json.dump(scene_gt_data[stream_name], f, indent=4)
+            with open(clip_stream_paths[f"scene_gt_info_{stream_name}"], "w") as f:
+                json.dump(scene_gt_info_data[stream_name], f, indent=4)
 
         # debug - check world to camera transformation
         # plot the camera pose in the world frame
